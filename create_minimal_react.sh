cbe3829c064cac021dd73ac9cd0f3f1ce2b506e6
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-02-14 12:40:46 +0700

    - remove acidental commented codes. - add guideline to execute script from github.

--- create_minimal_react.sh
+++ create_minimal_react.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# npm init -y
-# npm i react react-dom
-# npm i webpack webpack-cli webpack-dev-server @babel/core @babel/preset-react babel-loader --save-dev
-# wget https://raw.githubusercontent.com/nqminhuit/minimal-react/master/webpack.config.js
-# mkdir -p src/app
-# (cd src/ && wget https://raw.githubusercontent.com/nqminhuit/minimal-react/master/src/index.html)
-# (cd src/app/ && touch index.js && echo "console.log('app works!');" > index.js)
+# To execute this script without downloading it, run:
+# curl -s "https://raw.githubusercontent.com/nqminhuit/minimal-react/master/create_minimal_react.sh" | bash
+
+npm init -y
+npm i react react-dom
+npm i webpack webpack-cli webpack-dev-server @babel/core @babel/preset-react babel-loader --save-dev
+wget https://raw.githubusercontent.com/nqminhuit/minimal-react/master/webpack.config.js
+mkdir -p src/app
+(cd src/ && wget https://raw.githubusercontent.com/nqminhuit/minimal-react/master/src/index.html)
+(cd src/app/ && touch index.js && echo "console.log('app works!');" > index.js)
 echo "*** Please append these lines to package.json in \"scripts\" properties:"
 echo "================================================================================================================"
 echo "\"start\": \"npm run build\"",
