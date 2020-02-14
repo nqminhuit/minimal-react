497428f3c41e95432f91e1d2b001455e85fec40b
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-02-14 11:01:06 +0700

    - use button instead of text

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 
 class App extends React.Component {
+
+	alertUser() {
+		alert("you clicked the button!");
+	}
+
 	render() {
 		return (
 			<div className="container">
-				<div className="row">
-					<div className="col-xs-10 col-xs-offset-1">
-						<h1>Reacter!</h1>
-					</div>
+				<div className="col-xs-10 col-xs-offset-1">
+					<button onClick={this.alertUser.bind(this)} class="btn btn-primary" type="button">
+						Reacter
+					</button>
 				</div>
 			</div>
 		);
